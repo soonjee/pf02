febc264068573ac71cf2c5a50d67b32da834567c
soonji <youinxxs@naver.com>
2022-07-07 17:43:28 +0900

    20220707

--- js/pf02.js
+++ js/pf02.js
@@ -12,4 +12,30 @@ $(function () {
     $('.main .visual_control i:nth-child(2)').on('click', function () {
         $('.visual_slider').slick('slickNext')
     });
+
+    $('.coupon_slider').slick({
+        autoplay: true,
+        dots: true,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 3,
+    })
+
+    $('.coupon .coupon_control i:nth-child(1)').on('click', function () {
+        $('.coupon_slider').slick('slickPrev')
+    });
+    $('.coupon .coupon_control i:nth-child(2)').on('click', function () {
+        $('.coupon_slider').slick('slickNext')
+    });
+
+
+    $('.player').YTPlayer({
+        videoURL: 'https://youtu.be/XfdMal-fFWw',
+        containment: '.player',
+        autoPlay: true,
+        showControls: false,
+        playOnlyIfVisible: true,
+    });
+
+
 })
